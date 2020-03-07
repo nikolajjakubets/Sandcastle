d45af853b3601976f8cca567bbcdbcae1c749038
80036nd <40246027+80036nd@users.noreply.github.com>
2020-03-06 20:34:11 -0500

    Delete move.sh

--- move.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-Cd ~/Documents/iDroid
-rm -rf checkra1n.app
-rm -rf __MACOSX
-unzip checkra1n.app.zip
-rm -rf __MACOSX
-mv checkra1n.app /Applications
-cd /Applications/checkra1n.app/Contents/MacOS
-./checkra1n -c -V
